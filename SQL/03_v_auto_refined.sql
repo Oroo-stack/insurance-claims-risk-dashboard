0d321b14bd6774a025e87795fc9e7da80e8de50c
Christopher.Oroo <oroochristopher@gmail.com>
2026-07-11 21:45:16 +0300

    Update 03_v_auto_refined.sql

--- SQL/03_v_auto_refined.sql
+++ SQL/03_v_auto_refined.sql
@@ -1,5 +1,5 @@
 /* 
-  GOAL: Clean up missing text values and add flags for Power BI charts.
+  GOAL: Clean up missing text values, remove ghost columns, and add flags for Power BI.
   TARGET VIEW: `insurance-claims-risk-dash.auto_insurance_claims.v_auto_refined`
 */
 
@@ -15,20 +15,16 @@ CREATE OR REPLACE VIEW `insurance-claims-risk-dash.auto_insurance_claims.v_auto_
     
     total_claim_amount,
     
-    -- NEW: Claim Counter (Always 1)
-    -- This allows us to use SUM(claim_count) in Power BI to find Frequency.
+  
+    -- This allows one to use SUM(claim_count) in Power BI to find Frequency.
     1 AS claim_count,
-
-    -- NEW: Large Loss Flag 
+ 
     -- We flag claims over $50,000 (our audited average value) as a large loss
     CASE WHEN total_claim_amount > 50000 THEN 1 ELSE 0 END AS is_large_loss,
 
     -- Create a 1 or 0 flag for fraud
-   -- RIGHT (Converts True to 1 and False to 0 perfectly)
-CASE WHEN fraud_reported IS TRUE THEN 1 ELSE 0 END AS is_fraud_flag
-
+    CASE WHEN fraud_reported IS TRUE THEN 1 ELSE 0 END AS is_fraud_flag
 
   FROM 
     `insurance-claims-risk-dash.auto_insurance_claims.raw_auto_claims`
 );
-
